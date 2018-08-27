ccea3b662841d0366f0e4eeafed9377f0828e02a
Simon KP <skp@airtame.com>
2018-08-27 12:56:18 +1000

    Add ability to add friends and get group info

--- src/app.js
+++ src/app.js
@@ -57,11 +57,20 @@ function initPaySplitContract () {
         alert("No account is unlocked, please authorize an account on Metamask.")
       } else {
         PaySplitContract.deployed().then(function(instance) {
-          friends = [accounts[0], accounts[1]];
+          // TODO PASS FRIENDS FROM INPUT FIELD
+          //friends = [accounts[0], accounts[1]];
+          var friends = [];
+          var div = document.getElementById('here');
+          var input = div.getElementsByTagName('input');
+          for (var i = 0; i < input.length; i++ ) {
+            if(input[i].value != '') {
+              friends.push(input[i].value);
+            }
+          }
+          friends.push(accounts[0]); // add msg.sender to friends
           return instance.addGroup(friends, {from: accounts[0]});
         }).then(function(result) {
           console.log('Created new group')
-          //getSecondWrestlerAddress();
         }).catch(function(err) {
           console.log(err.message);
         });
@@ -79,10 +88,53 @@ function initPaySplitContract () {
         alert("No account is unlocked, please authorize an account on Metamask.")
       } else {
         PaySplitContract.deployed().then(function(instance) {
-          // TODO Do not hardcore. Pass the values from the page
           return instance.addExpense($("#group").val(), $("#expense").val(), {from: accounts[0]});
         }).then(function(result) {
           console.log('Added new expense')
+        }).catch(function(err) {
+          console.log(err.message);
+        });
+      }
+    }
+    });
+  }
+
+  function getGroupFriends () {
+    web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    } else {
+      if(accounts.length <= 0) {
+        alert("No account is unlocked, please authorize an account on Metamask.")
+      } else {
+        PaySplitContract.deployed().then(function(instance) {
+          return instance.getGroupFriends($("#group").val(), {from: accounts[0]});
+        }).then(function(result) {
+          for (i = 0; i < result.length; i++) { // go through friends
+            if (result[i] !== nullAddress) {
+              console.log(result[i])
+            }
+          }
+        }).catch(function(err) {
+          console.log(err.message);
+        });
+      }
+    }
+    });
+  }
+
+  function getGroup () {
+    web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    } else {
+      if(accounts.length <= 0) {
+        alert("No account is unlocked, please authorize an account on Metamask.")
+      } else {
+        PaySplitContract.deployed().then(function(instance) {
+          return instance.getGroup($("#group").val(), {from: accounts[0]});
+        }).then(function(result) {
+          console.log("total: ", result[0].toNumber(), "friends: ", result[1].toNumber(), "perUserCost: ", result[2].toNumber(), " finalised: ", result[3])
         }).catch(function(err) {
           console.log(err.message);
         });
@@ -98,7 +150,7 @@ function initPaySplitContract () {
         if (log.event == 'GroupCreated') {
           $("#eventsList").prepend('<li>' + log.event + ' with ID: ' + log.args.groupId.toNumber() + '</li>'); // Using JQuery, we will add new events to a list in our index.html
         } else if (log.event == 'ExpenseCreated') {
-          $("#eventsList").prepend('<li>' + log.event + ' for Group ID ' + log.args.groupId.toNumber() + ' From: ' + log.args.sender + ' Cost: $' + log.args.cost.toFixed(2) + '</li>'); // Using JQuery, we will add new events to a list in our index.html
+          $("#eventsList").prepend('<li>' + log.event + ' for Group ID ' + log.args.groupId.toNumber() + ' From: ' + log.args.sender + ' Cost: ' + log.args.cost.toFixed(2) + ' eth' + '</li>'); // Using JQuery, we will add new events to a list in our index.html
         }
       });
     }).catch(function(err) {
